a5767dca51644148111bb0dd45428ed991572682
Marinicheva Ekaterina <marri.nich@yandex.ru>
2022-01-24 19:29:49 +0300

    feat: add resolve of task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let answer = [];
+
+    if (Boolean(!matrix) || matrix.length < 1) {
+        return answer;
+    }
+
+    let arr = matrix.map((item, i) => {
+        if (i % 2 === 0) {
+            return item;
+        } else {
+            return item.reverse();
+        }
+    });
+
+    let str = arr.toString();
+    answer = str.split(',').map(item => +item);
+
+    return answer;
+};    
